76f5156cdc851f96ba14259d62732e6a37d4b521
Viet-Anh Le <>
2019-02-24 18:31:51 +0000

    Installed webpack-dev server

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,3 @@
 //Global controller
 import num from './test.js';
-console.log(`I imported ${num} from test.js`);
+console.log(`I imported ${num} from test.js, Hi Viet`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: ['./src/js/index.js'],
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
-    },    
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer:{
+        //the code we are going to shift to our client
+        contentBase:'./dist'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 };
