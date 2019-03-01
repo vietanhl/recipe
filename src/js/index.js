b629cd336d2d7313fa0a713d6b320a34de6e267d
Viet Le <viet-anh.le@hotmail.co.uk>
2019-03-01 17:53:51 +0000

    Implemented DOM and fixed query to be specific to the user's input

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,38 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
-async function getResults(query){
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-//axios == fetch, but is compatible with all browsers and automatically converts to JSON
-    const key = '82d1f6db9072ed56dce1d3b2c414322b';    
-    try{
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch(error) {
-        alert(error);
+/** Global state of the app
+ * - search object
+ * - current recipe
+ * - shopping list object
+ * - liked objects
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // get query from View
+    const query = searchView.getInput();
+    console.log(query);
+
+    if (query) {
+        // create new search obj and add to state
+        state.search = new Search(query);
+        // prepare UI for results
+
+        //search for recipes
+        await state.search.getResults();
+
+        //render results on UI
+        console.log(state.search.result);
     }
 }
 
-getResults('pizza');
+elements.searchForm.addEventListener('submit', e =>{ 
+    //prevent reload
+    e.preventDefault();
+    controlSearch();
+})
+
+const search = new Search ('pizza');
+search.getResults();
