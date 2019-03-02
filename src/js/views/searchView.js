0555acae12075207d1163ec7260301d0f745f7c4
Viet Le <viet-anh.le@hotmail.co.uk>
2019-03-02 18:10:02 +0000

    changed display for long recipe titles.

---
+++ src/js/views/searchView.js
@@ -0,0 +1,47 @@
+import {elements} from './base';
+
+export const getInput = () => {
+    return elements.searchInput.value;
+}
+export const clearInput = () => {
+    elements.searchInput.value = '';
+}
+export const clearResults = () => {
+    elements.searchResList.innerHTML = '';
+}
+// display for long texts
+const limitRecipeTitle = (title, limit = 17 ) => {
+    const newTitle = [];
+    if (title.length > limit ){
+        title.split(' ').reduce((acc, curr) => {
+            if (acc + curr.length <= limit) {
+                newTitle.push(curr);
+            }
+            return acc + curr.length;
+        }, 0);
+        return `${newTitle.join(' ')} ...`;
+    }
+
+    return title;
+}
+const renderRecipe = recipe => {
+    const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.recipe_id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image_url}" alt="${recipe.title}">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                    <p class="results__author">${recipe.publisher}</p>
+                </div>
+            </a>
+        </li>
+    `;
+    //render this in html
+    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+}
+
+export const renderResults = recipes => {
+    recipes.forEach(renderRecipe);
+}
